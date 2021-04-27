fe34c63f681a20819c6efa47540a14972a98e03f
MeG Bots <70581186+Gowtham0625@users.noreply.github.com>
2021-04-27 20:15:33 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -36,9 +36,9 @@ async def help_message_f(client, message):
     #channel_id = str(AUTH_CHANNEL)[4:]
     #message_id = 99
     # display the /help
-    DEV = os.environ.get("DEV", "@Gillz_13")
+    DEV = os.environ.get("DEV", "@MeGBots")
     button = []
-    link = "https://telegra.ph/Help-Message-03-12"
+    link = "https://t.me/MeGleech/7"
     button.append([pyrogram.InlineKeyboardButton(text="Click to Read", url=f"{link}")])
     button_markup = pyrogram.InlineKeyboardMarkup(button)
     await message.reply_text(f"**Hello** 👾 !\n__This is Telegram Leech bot 🧲__ \n__Click Below to know how to use me 📝__\n\n**Developer 👨🏻‍💻**: {DEV}",reply_markup=button_markup)
