2ff1ad902042883b6fef270d55bf68a0f1b9f7c0
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-04-28 13:41:43 +0530

    Update display_progress_g.py

--- tobrot/helper_funcs/display_progress_g.py
+++ tobrot/helper_funcs/display_progress_g.py
@@ -40,12 +40,12 @@ async def progress_for_pyrogram_g(
         elapsed_time = time_formatter(milliseconds=elapsed_time)
         estimated_total_time = time_formatter(milliseconds=estimated_total_time)
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
+        progress = "╭────── ⌊ Uploaded : [ {2} ] 📤 ⌉</b>\n<b>│ </b>\n<b>├〚 {0}{1} 〛</b>\n<b>│</b>".format(
             ''.join([FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
             ''.join([UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\n".format(
+        tmp = progress + "\n<b>├ Uploaded : {0}</b>\n<b>├ Total : {1}</b>\n</b>├ Speed : {2}</b>\n<b>├ ETA : {3}</b>\n│</b>\n<b>│</b>\n<b>│   ╔╦══• •✠•❀  ❀•✠• •══╦╗</b>\n<b>╰─⟰⚡️ᴜsɪɴɢ ᴇɴɢɪɴᴇ ᴀʀɪᴀ2 ⟱ </b>\n<b>      ╚╩══• •✠•❀  ❀•✠• •══╩╝</b>".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
