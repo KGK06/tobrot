3514a902450d3f2dfa9024efdc7c991fbc591534
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-04-28 10:57:48 +0530

    Done ✅😏

--- tobrot/plugins/custom_thumbnail.py
+++ tobrot/plugins/custom_thumbnail.py
@@ -1,23 +1,15 @@
-"""ThumbNail utilities, © @AnyDLBot"""
-
-
 import os
-
-from tobrot import DOWNLOAD_LOCATION
 
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 from PIL import Image
+from tobrot import DOWNLOAD_LOCATION
 
 
 async def save_thumb_nail(client, message):
-    thumbnail_location = os.path.join(
-        DOWNLOAD_LOCATION,
-        "thumbnails"
-    )
+    thumbnail_location = os.path.join(DOWNLOAD_LOCATION, "thumbnails")
     thumb_image_path = os.path.join(
-        thumbnail_location,
-        str(message.from_user.id) + ".jpg"
+        thumbnail_location, str(message.from_user.id) + ".jpg"
     )
     ismgs = await message.reply_text("processing ...")
     if message.reply_to_message is not None:
@@ -25,8 +17,7 @@ async def save_thumb_nail(client, message):
             os.makedirs(thumbnail_location)
         download_location = thumbnail_location + "/"
         downloaded_file_name = await client.download_media(
-            message=message.reply_to_message,
-            file_name=download_location
+            message=message.reply_to_message, file_name=download_location
         )
         # https://stackoverflow.com/a/21669827/4723940
         Image.open(downloaded_file_name).convert("RGB").save(downloaded_file_name)
@@ -44,11 +35,11 @@ async def save_thumb_nail(client, message):
         # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
         os.remove(downloaded_file_name)
         await ismgs.edit(
-            "<b>🏙 Custom video / file thumbnail saved.</b>\n"
-            + "<b>This image will be used in the upload, till /clearthumb.</b>"
+            "<b>🎆 Custom video / file thumbnail saved.</b>\n"
+            + "<b>This image will be used in the upload, till /clearthumb</b>."
         )
     else:
-        await message.edit("<b>❌ Reply to a photo to save custom thumbnail")
+        await ismgs.edit("<b>❌ Reply to a photo to save custom thumbnail</b>")
 
 
 async def clear_thumb_nail(client, message):
