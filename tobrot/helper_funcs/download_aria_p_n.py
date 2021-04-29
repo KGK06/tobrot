eefe6a853107ff9d461f0b9da138946596a86150
Gowtham Kumar <72903548+KGK06@users.noreply.github.com>
2021-04-29 10:12:00 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -495,7 +495,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                    msg += f"\n<b>├ Info</b> : <b>[ P : {file.connections} || S : {file.num_seeders} ]</b>"
 
                 # msg += f"\n<b>Status</b> : <code>{file.status}</code>"
-                msg += f"\n<b>├ ETA</b> :  <code>{file.eta_string()}</code>" +"\n\n<b>│</b>\n<b>│   ╔╦══• •✠•❀  ❀•✠• •══╦╗</b>"
+                msg += f"\n<b>├ ETA</b> :  <code>{file.eta_string()}</code>" +"\n<b>│</b>\n<b>│   ╔╦══• •✠•❀  ❀•✠• •══╦╗</b>"
                 msg += "\n<b>╰─⟰⚡️ᴜsɪɴɢ ᴇɴɢɪɴᴇ ᴀʀɪᴀ2 ⟱ </b>\n<b>      ╚╩══• •✠•❀  ❀•✠• •══╩╝</b>"
                 inline_keyboard = []
                 ikeyboard = []
